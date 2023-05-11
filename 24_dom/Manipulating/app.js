cee5630f332d479360977b55fb01eecc02ec1871
uberphoebus <uberphoebus@gmail.com>
2023-05-12 08:45:37 +0900

    add 24_dom

---
+++ 24_dom/Manipulating/app.js
@@ -0,0 +1,13 @@
+const allLinks = document.querySelectorAll("a");
+for (let link of allLinks) {
+    link.innerText = "I AM A LINK!!!!";
+}
+
+const temp = document.querySelector("h1").innerHTML;
+
+
+// for (let link of allLinks) {
+//     link.style.color = 'rgb(0, 108, 134)';
+//     link.style.textDecorationColor = 'magenta';
+//     link.style.textDecorationStyle = 'wavy'
+// }
