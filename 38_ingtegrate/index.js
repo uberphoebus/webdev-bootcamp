26a8090992664d40aa26b0bc014fc4c3fa513e53
uberphoeus <uberphoeus@gmail.com>
2023-05-27 09:04:05 +0900

    add 38_integrate

--- 38_ingtegrate/index.js
+++ 38_ingtegrate/index.js
@@ -1,8 +1,9 @@
-const express = require("express");
-const app = express();
 const path = require("path");
-const mongoose = require("mongoose");
 const env = require("dotenv");
+const express = require("express");
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
+const app = express();
 const Product = require("./models/product");
 
 // database settings
@@ -20,39 +21,58 @@ mongoose
     });
 
 // app settings
+app.use(methodOverride("_method"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
+const log_routes = (req, res, action = "") => {
+    console.log(
+        `[${"request".padEnd(7)}] ${req.method.padEnd(4)} ${req.url} ${action}`
+    );
+};
+
 // app routes
 app.get("/products", async (req, res) => {
-    console.log(`[${"request".padEnd(7)}] ${"GET".padEnd(4)} /products`);
+    log_routes(req, res);
+    // console.log(`[${"request".padEnd(7)}] ${"GET".padEnd(4)} /products`);
     const products = await Product.find({});
     res.render("products/index", { products });
 });
 
 app.get("/products/new", (req, res) => {
-    console.log(`[${"request".padEnd(7)}] ${"GET".padEnd(4)} /products/new`);
+    log_routes(req, res);
     res.render("products/new");
 });
 
 app.post("/products", async (req, res) => {
-    console.log(`[${"request".padEnd(7)}] ${"POST".padEnd(4)} /products`);
+    log_routes(req, res);
     const newProduct = new Product(req.body);
     await newProduct.save();
-    console.log(
-        `[${"request".padEnd(7)}] ${"POST".padEnd(4)} /products saved : ${
-            newProduct.name
-        }`
-    );
+    log_routes(req, res, `saved: ${newProduct.name}`);
     res.redirect(`products/${newProduct._id}`);
 });
 
 app.get("/products/:id", async (req, res) => {
     const { id } = req.params;
-    console.log(`[${"request".padEnd(7)}] ${"GET".padEnd(4)} /products/${id}`);
+    log_routes(req, res);
     const product = await Product.findById(id);
     res.render("products/show", { product });
+});
+
+app.get("/products/:id/edit", async (req, res) => {
+    const { id } = req.params;
+    log_routes(req, res);
+    const product = await Product.findById(id);
+    res.render("products/edit", { product });
+});
+
+app.put("/products/:id", async (req, res) => {
+    const { id } = req.params;
+    log_routes(req, res);
+    Product.findByIdAndUpdate(id, req.body, { runValidators: true });
+    log_routes(req, res, `updated: ${req.body.name}`);
+    res.send("PUT");
 });
 
 // app listen
