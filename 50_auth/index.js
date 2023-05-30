c320f5d68b9409e687eaa0257294380f5ba489fc
uberphoeus <uberphoeus@gmail.com>
2023-05-30 20:58:28 +0900

    refactor models

--- 50_auth/index.js
+++ 50_auth/index.js
@@ -41,12 +41,7 @@ app.get("/", (req, res) => {
 
 app.post("/register", async (req, res) => {
     const { username, password } = req.body;
-    const hash = await bcrypt.hash(password, 12);
-    console.log(hash);
-    const user = new User({
-        username: username,
-        password: hash,
-    });
+    const user = new User({ username, password });
     await user.save();
     req.session.user_id = user._id;
     res.redirect("/");
@@ -58,10 +53,9 @@ app.get("/login", (req, res) => {
 
 app.post("/login", async (req, res) => {
     const { username, password } = req.body;
-    const user = await User.findOne({ username: username });
-    const validPassword = await bcrypt.compare(password, user.password);
-    if (validPassword) {
-        req.session.user_id = user._id;
+    const foundUser = await User.findAndValidate(username, password);
+    if (foundUser) {
+        req.session.user_id = foundUser._id;
         res.redirect("/secret");
     } else {
         res.redirect("/login");
--- 50_auth/models/user.js
+++ 50_auth/models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
@@ -8,6 +10,17 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: [true, "Password cannot be blank"],
     },
+});
+
+userSchema.statics.findAndValidate = async function (username, password) {
+    const foundUser = await this.findOne({ username });
+    const isValid = await bcrypt.compare(password, foundUser.password);
+    return isValid ? foundUser : false;
+};
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 12);
 });
 
 module.exports = mongoose.model("User", userSchema);
