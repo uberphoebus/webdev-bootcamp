9bc4dcc333ac4916bbe9026964df70ecd4a78a56
uberphoeus <uberphoeus@gmail.com>
2023-05-30 20:33:01 +0900

    add requireLogin middleware

--- 50_auth/index.js
+++ 50_auth/index.js
@@ -24,6 +24,13 @@ app.set("views", "views");
 app.use(express.urlencoded({ extended: true }));
 app.use(session({ secret: "notagoodsecret" }));
 
+const requireLogin = (req, res, next) => {
+    if (!req.session.user_id) {
+        return res.redirect("/login");
+    }
+    next();
+};
+
 app.get("/register", (req, res) => {
     res.render("register");
 });
@@ -67,12 +74,8 @@ app.post("/logout", (req, res) => {
     res.redirect("/login");
 });
 
-app.get("/secret", (req, res) => {
-    if (!req.session.user_id) {
-        res.redirect("/login");
-    } else {
-        res.render("secret");
-    }
+app.get("/secret", requireLogin, (req, res) => {
+    res.render("secret");
 });
 
 app.listen(3000, (req, res) => {
